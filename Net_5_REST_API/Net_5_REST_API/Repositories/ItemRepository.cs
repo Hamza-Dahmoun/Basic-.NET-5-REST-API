40b90d02a928c448cd34351ee4dc42d37246c9da
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-30 05:27:22 +0100

    add Repositories/ItemRepository.cs

---
+++ Net_5_REST_API/Net_5_REST_API/Repositories/ItemRepository.cs
@@ -0,0 +1,28 @@
+﻿using Net_5_REST_API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net_5_REST_API.Repositories
+{
+    public class ItemRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item {Id=Guid.NewGuid(), Name="Potion", Price=9, CreationDate=DateTime.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreationDate = DateTime.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreationDate = DateTime.UtcNow },
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid Id)
+        {
+            return items.Where(i => i.Id == Id).SingleOrDefault();
+        }
+    }
+}
