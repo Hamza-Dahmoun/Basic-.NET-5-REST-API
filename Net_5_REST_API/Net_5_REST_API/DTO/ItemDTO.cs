ccd800b4091c9191959bcafe5dac9bd3e7c14bb1
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-30 08:42:43 +0100

    add ItemDTO class

---
+++ Net_5_REST_API/Net_5_REST_API/DTO/ItemDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net_5_REST_API.DTO
+{
+    public class ItemDTO
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public DateTime CreationDate { get; init; }
+    }
+}
