d723effa920b6723030061404755bea4c3bac8df
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-30 10:47:27 +0100

    add UpdatetemDTO record type

---
+++ Net_5_REST_API/Net_5_REST_API/DTO/UpdateItemDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net_5_REST_API.DTO
+{
+    public record UpdateItemDTO
+    {
+        [Required]
+        public string Name { get; init; }
+        [Range(1, 1000)]
+        public decimal Price { get; init; }
+    }
+}
