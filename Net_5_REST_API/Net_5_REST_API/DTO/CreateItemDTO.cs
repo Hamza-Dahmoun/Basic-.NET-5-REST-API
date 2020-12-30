5e909b295f228fb9233efd6052074492bdda8000
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-30 10:29:36 +0100

    annotate CreateItemDTO properties

--- Net_5_REST_API/Net_5_REST_API/DTO/CreateItemDTO.cs
+++ Net_5_REST_API/Net_5_REST_API/DTO/CreateItemDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace Net_5_REST_API.DTO
 {
     public record CreateItemDTO
     {
+        [Required]
         public string Name { get; init; }
+        [Range(1, 1000)]
         public decimal Price { get; init; }
     }
 }
