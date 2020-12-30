969344c4e219df7a44069463d1a64ce428024865
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-30 05:43:20 +0100

    add ItemController.cs with 2 get methods

---
+++ Net_5_REST_API/Net_5_REST_API/Controllers/ItemController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Net_5_REST_API.Entities;
+using Net_5_REST_API.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net_5_REST_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly ItemRepository repository;
+
+        public ItemController()
+        {
+            repository = new ItemRepository();
+        }
+
+        //Item
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            return repository.GetItems();
+        }
+
+        //Item/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return item;
+            }
+        }
+    }
+}
